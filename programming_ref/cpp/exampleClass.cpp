f81fe31fa61bb5e93d3d0d623f5daaf1587a46b9
Joe Loy <josephl@schulerprogram.org>
2023-04-04 12:18:06 -0500

    comment cpp ref

--- programming_ref/cpp/exampleClass.cpp
+++ programming_ref/cpp/exampleClass.cpp
@@ -1,10 +1,26 @@
+// Must include libraries that are being used in the file
 #include <iostream>
 #include <string>
+
+// Note that you must include the header file of your class
+// in order to use it in other files
 #include "exampleClass.hpp"
 
+// Constructor method
+// className::className(args) {
+// ...
+// }
+// Notice there is no return type for the constructor
 exampleClass::exampleClass() {
     std::cout << "Class has been created" << std::endl;
 }
+
+// Member functions
+// Notice that member functions require return types
+// as well as namespace declarations before function names
+// returnType className::functionName(args) {
+// ...
+// }
 
 void exampleClass::setInt(int x) {
     var1 = x;
--- programming_ref/cpp/exampleClass.hpp
+++ programming_ref/cpp/exampleClass.hpp
@@ -1,18 +1,22 @@
+// Header files should start with #ifndef and #define
 #ifndef EXAMPLECLASS_H
 #define EXAMPLECLASS_H
 
 #include <string>
 
 class exampleClass {
+    // These are functions and variables that can be accessed from outside the class
     public:
         exampleClass();
         void setInt(int x);
         void setStr(std::string x);
         void printState();
-    
+    // These are functions and variables that can only be accessed from inside this class
+    // and from other classes that inherit from this class
     private:
         int var1;
         std::string var2;
-};
+}; // Do not forget to put a semicolon after you
 
+// Header files should end with #endif
 #endif
